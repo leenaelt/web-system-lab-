5d822f63418a3ce2088e038e7d297032f1852449
Leena Eltariefi <leenaeltariefi@LeenaElt.local>
2026-02-19 07:57:25 +0300

    Upload lab3 SQL assignment

---
+++ db_connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "university";   // تأكدي الاسم مطابق لقاعدة البيانات
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
