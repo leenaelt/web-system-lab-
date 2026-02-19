fbf630cc6ed1159fcd9885038a572f9968f6ee9e
leenaelt <168964450+leenaelt@users.noreply.github.com>
2026-02-19 08:01:30 +0300

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -2,11 +2,11 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "university";   // تأكدي الاسم مطابق لقاعدة البيانات
+$dbname = "university";   
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
